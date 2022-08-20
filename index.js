1c70f7b01cde2e500dab6927a5785665f7abcb23
AbdullahT <83435812+AbdullahTh7@users.noreply.github.com>
2022-08-20 21:03:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$('.number').one("click", function(){
+    
+    this.setAttribute("style","color: white; background-color: hsl(25, 97%, 53%);");
+    var rate = $(this).html();
+    $('#rate').html(rate);
+});
+
+$('.submit').one("click", function(){
+
+    this.setAttribute("style", "color: hsl(25, 97%, 53%); background-color: white;");
+    setTimeout(function(){
+        document.getElementById("css").href="style2.css";
+        document.getElementById("favicon").src="images/illustration-thank-you.svg";
+        $('h3').text('Thank you!');
+        $('p').text('We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!');
+        $(".number, .sumbit").text('');
+        x.style.display = "block";
+    },1000);
+
+    
+    
+});
+
+
+var x = document.getElementById("selected");
+x.style.display = "none";
